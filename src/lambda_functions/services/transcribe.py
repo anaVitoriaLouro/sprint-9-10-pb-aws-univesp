0797671f4972d7d1dcc6725c664034ddcbdb02fe
M Vinicios Nativo <onativo@outlook.com>
2023-06-12 22:46:24 -0300

    :recycle: refactor: funções estão mais clean

--- src/lambda_functions/services/transcribe.py
+++ src/lambda_functions/services/transcribe.py
@@ -1,16 +1,18 @@
 import boto3
-from utils.generate_id import generate_random_uuid
+import uuid
 
 # Create a Transcribe client
 transcribe = boto3.client('transcribe')
 
 def transcribe_audio(bucket_name, object_key, uuid):
+    
+
     try:
         # Extract the media format from the object_key
         media_format = object_key.split('.')[-1]
 
-        hash_name = generate_random_uuid()
-
+        hash_name = str(uuid.uuid4().hex)
+        
         # Configure the transcription job
         transcribe_job = {
             'TranscriptionJobName': hash_name,
@@ -28,17 +30,19 @@ def transcribe_audio(bucket_name, object_key, uuid):
         # Wait for the transcription job to complete or fail
         while True:
             status = transcribe.get_transcription_job(TranscriptionJobName=response['TranscriptionJob']['TranscriptionJobName'])
-            if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
-                print('Transcription Job has failed')
+            if status['TranscriptionJob']['TranscriptionJobStatus'] == 'FAILED':
+                failure_reason = status['TranscriptionJob']['FailureReason']
+                print(f'TranscriptionJob has failed unexpectedly bescause: {failure_reason}.')
                 break
 
         if status['TranscriptionJob']['TranscriptionJobStatus'] == 'COMPLETED':
             transcribed_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+            print('TranscriptionJob has been completed!')
             transcribed_text = boto3.client('s3').get_object(Bucket=bucket_name, Key=transcribed_uri.split('/')[-1])['Body'].read().decode('utf-8')
             return transcribed_text
         else:
             failure_reason = status['TranscriptionJob']['FailureReason']
-            raise Exception(f'Transcription failed. Job status: {status["TranscriptionJob"]["TranscriptionJobStatus"]}. Failure reason: {failure_reason}')
+            raise Exception(f'Transcription failed with delight. Job status: {status["TranscriptionJob"]["TranscriptionJobStatus"]}. And the failure reason is: {failure_reason}')
 
     except Exception as e:
         raise Exception(f'Transcription failed with error: {str(e)}')
