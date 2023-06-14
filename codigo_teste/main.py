4302dcc6c9bfb8b9136942cd44e1a8ee591343c7
Ana Vitória Louro Navili <va014863@gmail.com>
2023-06-14 16:42:01 -0300

    feat: Criando pasta com código teste que executa e faz a interação com o chatbot.

--- codigo_teste/main.py
+++ codigo_teste/main.py
@@ -1,6 +1,10 @@
+import asyncio
 import boto3
+from upload_utils import upload_file_to_s3
+from transcription_utils import wait_for_transcription_completion
 
-#Cria instâncias dos clientes fora da função lambda (escopo global)
+# Cria instâncias dos clientes fora da função lambda (escopo global)
+session = boto3.Session()
 lex_runtime = session.client('lex-runtime')
 rekognition = session.client('rekognition')
 translate = session.client('translate')
@@ -10,7 +14,6 @@ transcribe = session.client('transcribe')
 input_text = input("Digite a entrada do usuário: ")
 
 # Processamento da entrada
-#Obs: Colocar informações referentes ao bot
 response = lex_runtime.postText(
     botName='YourBotName',
     botAlias='YourBotAlias',
@@ -18,17 +21,10 @@ response = lex_runtime.postText(
     inputText=input_text
 )
 
-# Verifica se há uma imagem ou áudio na entrada
+# Verifica se há uma imagem na entrada
 if response.get('dialogState') == 'ElicitSlot' and response.get('slotToElicit') == 'ImageSlot':
     # Faz o upload da imagem para o Amazon S3 e obtém a URL
-    # Trocar 'bucket-name' para o nome do bucket
-    s3_client = session.client('s3')
-    s3_client.upload_file('path/to/image.jpg', 'bucket-name', 'image.jpg')
-    image_url = s3_client.generate_presigned_url(
-        'get_object',
-        Params={'Bucket': 'bucket-name', 'Key': 'image.jpg'},
-         ExpiresIn=3600   #Tempo de expiração da URL em segundos
-    )
+    image_url = upload_file_to_s3('path/to/image.jpg', 'bucket-name', 'image.jpg')
 
     # Use o Amazon Rekognition para analisar a imagem
     rekognition_response = rekognition.detect_text(
@@ -46,38 +42,30 @@ if response.get('dialogState') == 'ElicitSlot' and response.get('slotToElicit') 
 # Verifica se há áudio na entrada
 if response.get('dialogState') == 'ElicitSlot' and response.get('slotToElicit') == 'AudioSlot':
     # Faz o upload do áudio para o Amazon S3 e obtém a URL
-    # Trocar 'bucket-name' para o nome do bucket
-    s3_client = session.client('s3')
-    s3_client.upload_file('path/to/audio.wav', 'bucket-name', 'audio.wav')
-    audio_url = s3_client.generate_presigned_url(
-        'get_object',
-        Params={'Bucket': 'bucket-name', 'Key': 'audio.wav'},
-        ExpiresIn=3600  # Tempo de expiração da URL em segundos
-    )
+    audio_url = upload_file_to_s3('path/to/audio.wav', 'bucket-name', 'audio.wav')
 
     # Use o Amazon Transcribe para transcrever o áudio
     transcribe_response = transcribe.start_transcription_job(
         TranscriptionJobName='transcription-job',
         LanguageCode='pt',
         Media={'MediaFileUri': audio_url},
-        # substituir pelo nome do bucket onde salvar a transcrição
         OutputBucketName='bucket-name'
     )
 
-    # Aguarde a conclusão da transcrição
-    transcribe_client = session.client('transcribe')
-    while True:
-        job_status = transcribe_client.get_transcription_job(
-            TranscriptionJobName='transcription-job')
-        if job_status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
-            break
+    # Aguarda a conclusão da transcrição de forma assíncrona
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(wait_for_transcription_completion('transcription-job'))
 
     # Obtém o resultado da transcrição
-    transcription_result_url = job_status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+    transcription_result_url = transcribe.get_transcription_job(
+        TranscriptionJobName='transcription-job'
+    )['TranscriptionJob']['Transcript']['TranscriptFileUri']
+
     transcription_response = session.client('s3').get_object(
-        Bucket='bucket-name', Key=transcription_result_url[transcription_result_url.find('/')+1:])['Body'].read().decode('utf-8')
-    transcribed_text = json.loads(transcription_response)[
-        'results']['transcripts'][0]['transcript']
+        Bucket='bucket-name',
+        Key=transcription_result_url[transcription_result_url.find('/')+1:]
+    )['Body'].read().decode('utf-8')
+    transcribed_text = json.loads(transcription_response)['results']['transcripts'][0]['transcript']
 
     # Usa o texto transcrito como entrada para o Amazon Lex Runtime Service
     response = lex_runtime.postText(
