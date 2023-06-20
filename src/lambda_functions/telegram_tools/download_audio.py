88c272448754b9f2d59175a2fa9e3c23ca7e7760
M Vinicios Nativo <onativo@outlook.com>
2023-06-20 10:48:54 -0300

    :recycle: refactor: modificando funcao de download de audio

--- src/lambda_functions/telegram_tools/download_audio.py
+++ src/lambda_functions/telegram_tools/download_audio.py
@@ -1,34 +1,30 @@
-import boto3
 import requests
 import json
 import os
 from io import BytesIO
-from pydub import AudioSegment
+from utils.upload_to_s3 import upload_file_to_s3
 
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 AUDIO_INPUT_BUCKET_NAME = os.environ['AUDIO_INPUT_BUCKET_NAME']
 
 def download_audio(file_id, file_unique_id):
-    # This gets the "file_path" of the specified file
-    response = requests.get(f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getFile?file_id={file_id}")
-    audiofile_path = response.json()['result']['file_path']
+    try:
+        # This gets the "file_path" of the specified file
+        response = requests.get(f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getFile?file_id={file_id}")
+        # Check if there was a request error
+        response.raise_for_status()
+        audiofile_path = response.json()['result']['file_path']
 
-    # Now this downloads the file passed by the user
-    audio_file = requests.get(f"https://api.telegram.org/file/bot{TELEGRAM_TOKEN}/{audiofile_path}").content
+        # Now this downloads the file passed by the user
+        audio_file = requests.get(f"https://api.telegram.org/file/bot{TELEGRAM_TOKEN}/{audiofile_path}").content
+        
+        # Use the given file_unique_id to store the file in a S3Bucket
+        audio_file_key = (f'{file_unique_id}.mp3')
+        
+        upload_file_to_s3(audio_file, AUDIO_INPUT_BUCKET_NAME, audio_file_key)
 
-    # Convert the default '.oga' format to '.mp3'
-    audio = AudioSegment.from_file(audio_file, format="oga")
-    mp3_data = audio.export(format="mp3")
-    mp3_audio = AudioSegment.from_file(mp3_data, format="mp3")
-
-    # Use the given file_unique_id to store the file in a S3Bucket
-    audio_file_key = (f'input_audios/{file_unique_id}.mp3')
-
-    # Connects to the S3 client and uploads the audio to it
-    s3_client = boto3.client('s3')
-    s3_client.put_object(Bucket=AUDIO_INPUT_BUCKET_NAME, Key=audio_file_key, Body=mp3_audio)
-
-    # Print the url from that file to see if was uploaded correctly
-    print(url = s3_client.generate_presigned_url('get_object', Params={'Bucket': AUDIO_INPUT_BUCKET_NAME, 'Key': audio_file_key}, ExpiresIn=1200))
-
-    return audio_file_key
+        print(audio_file_key)
+        return audio_file_key
+    
+    except Exception as e:
+        print(f'Impossible to donwload audio file from Telegram: {e}')
